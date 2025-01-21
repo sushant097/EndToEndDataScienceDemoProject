1a8dc1a62b8f00ad30b8213fedb86b795f93aa40
Sushant Gautam <sushant1997gautam@gmail.com>
2025-01-21 17:41:51 -0600

    added template of the project for automatic files and folders creation

---
+++ template.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+import logging
+
+
+project_name="datascienceDemoProject"
+
+list_of_files = [
+    ".gtihub/workflows/.gitkeep", 
+    "",
+    "", 
+    "",
+    "", 
+    ""
+]
+
+logging.basicConfig()
