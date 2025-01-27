27e33f388793da854de8a3c0653fd7f597420b76
Sushant Gautam <sushant1997gautam@gmail.com>
2025-01-26 19:59:15 -0600

    completed data ingestion

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # EndToEndDataScienceDemoProject
 Demo Data Science project with complete End-to-End Template
+
+### Workflows--ML Pipeline
+
+1. Data Ingestion
+2. Data Validation
+3. Data Transformatoin
+4. Model Trainer
+5. Model Evaluation
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yam;
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline
+8. Update the main.py
+
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from src.datascienceDemo import logger
+from src.datascienceDemo.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Welcome to our custom logging data science")
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
 
