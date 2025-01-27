93da9cac2ce52529a5e32ba3bdf3d80eb3c63e43
Sushant Gautam <sushant1997gautam@gmail.com>
2025-01-26 20:42:30 -0600

    Data Validation phase module

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.datascienceDemo import logger
 from src.datascienceDemo.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.datascienceDemo.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "DATA INGESTION STAGE"
@@ -13,6 +14,21 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "DATA VALIDATION STAGE"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
 
 
 
---
+++ src/datascienceDemo/pipeline/data_validation_pipeline.py
@@ -0,0 +1,30 @@
+from src.datascienceDemo.config.configuration import ConfigurationManager
+from src.datascienceDemo.components.data_validation import DataValidation
+from src.datascienceDemo import logger
+
+
+STAGE_NAME = "Data Validation Stage"
+
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+
+    def initiate_data_validation(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.initiate_data_validation()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
